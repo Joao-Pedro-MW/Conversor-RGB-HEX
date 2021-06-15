0e45da5a426a0353a236e53f7a8939e3ac194bf6
João Pedro Müller <80231608+Joao-Pedro-MW@users.noreply.github.com>
2021-06-15 10:31:23 -0300

    Update Conversor RGB.py

--- Conversor RGB.py
+++ Conversor RGB.py
@@ -7,9 +7,10 @@ def rgb(r, g, b):
     if g<0 : g= 0
     if b<0 : b= 0
     
-    r = '{:02X}'.format(r)
-    g = '{:02X}'.format(g)
+    r = '{:02X}'.format(r) #converte para Hexadecimal com 2 casas decimais, caso precise
+    g = '{:02X}'.format(g) #é adicionado um zero a esquerda do número
     b = '{:02X}'.format(b)
     a = '{0}{1}{2}'.format(r,g,b)
     a = a.upper() 
     return a
+
